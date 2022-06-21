f7b96b8fbdaeab6130eba4b2c8a473ec360679a9
Jose Hurtado <hello@hurtadojose.com>
2022-06-21 23:25:17 +0000

    New Updates HTML, CSS and JS Filews

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "best", "unique"];
+  let adj = ["great", "big", "amazing", "funny", "sleepy"];
+  let noun = ["jogger", "racoon", "motherinlaw", "elephant", "batman"];
+  let dtl = [".com", ".net", ".org", ".us", ".biz", ".co", ".academy"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dtl.length; l++) {
+          document.querySelector("#domain_name").innerHTML +=
+            '<p class="text-secondary"><h3> www.' +
+            pronoun[i] +
+            adj[j] +
+            noun[k] +
+            dtl[l] +
+            " </h3></p> <hr>";
+        }
+      }
+    }
+  }
 };
